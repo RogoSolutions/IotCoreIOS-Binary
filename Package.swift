4e6573741472de2953c8e39a4bf71255f8a634ff
Tuan ANh <anhdt14@gmail.com>
2026-08-27 16:44:05 +0700

    Release 0.0.24
    - Updated Package.swift with new binary target URL and checksum
    - Updated podspec
    - Updated Sample App
    
    SHA256: f3e159342be4f7eba1886d449c56ee70e6a3a00acc2189ac5e9de0b339c5a570

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IotCoreIOSBinary",
-            url: "https://github.com/RogoSolutions/IotCoreIOS-Binary/releases/download/0.0.23/IotCoreIOS-0.0.23.xcframework.zip",
-            checksum: "385e0c5f0e6b3596c420f3ac0ad9f3668774b31f7833d27508b332fbc060a1f7"
+            url: "https://github.com/RogoSolutions/IotCoreIOS-Binary/releases/download/0.0.24/IotCoreIOS-0.0.24.xcframework.zip",
+            checksum: "f3e159342be4f7eba1886d449c56ee70e6a3a00acc2189ac5e9de0b339c5a570"
         ),
         .binaryTarget(
             name: "CocoaMQTTBinary",
-            url: "https://github.com/RogoSolutions/IotCoreIOS-Binary/releases/download/0.0.23/CocoaMQTT-0.0.23.xcframework.zip",
+            url: "https://github.com/RogoSolutions/IotCoreIOS-Binary/releases/download/0.0.24/CocoaMQTT-0.0.24.xcframework.zip",
             checksum: "88b0703ade4f496c20518672bc90758135e65b362bd6f575d7fcc6634dd3071d"
         ),
         .target(
